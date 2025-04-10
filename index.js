db7378f352a3f9bf4b025fe662dc08a46e6d866d
Lusquinha <lusquinha1204@outlook.com>
2025-04-09 21:20:06 -0300

    ajustes mobile

--- index.js
+++ index.js
@@ -1,3 +1,21 @@
+// Scroll smooth
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+    anchor.addEventListener('click', function (e) {
+      e.preventDefault();
+      
+      const targetId = this.getAttribute('href');
+      const targetElement = document.querySelector(targetId);
+      
+      if (targetElement) {
+        targetElement.scrollIntoView({
+          behavior: 'smooth',
+          block: 'start'
+        });
+      }
+    });
+  });
+
+
 // Função do topo
 
 /*const toggleButton = document.getElementById('toggleButton');
