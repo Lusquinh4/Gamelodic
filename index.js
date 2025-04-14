b7038282d4c1d0dd1bfa9491d3a533affcc9ed56
lusca <lusquinha1204@outlook.com>
2025-04-14 10:55:49 -0300

    ajuste

--- index.js
+++ index.js
@@ -1,110 +1,101 @@
 // Scroll smooth
-document.querySelectorAll('a[href^="#"]').forEach(anchor => {
-    anchor.addEventListener('click', function (e) {
-      e.preventDefault();
-      
-      const targetId = this.getAttribute('href');
-      const targetElement = document.querySelector(targetId);
-      
-      if (targetElement) {
-        targetElement.scrollIntoView({
-          behavior: 'smooth',
-          block: 'start'
-        });
-      }
-    });
+document.querySelectorAll('a[href^="#"]').forEach((anchor) => {
+  anchor.addEventListener("click", function (e) {
+    e.preventDefault();
+
+    const targetId = this.getAttribute("href");
+    const targetElement = document.querySelector(targetId);
+
+    if (targetElement) {
+      targetElement.scrollIntoView({
+        behavior: "smooth",
+        block: "start",
+      });
+    }
   });
-
-
-// Função do topo
-
-/*const toggleButton = document.getElementById('toggleButton');
-toggleButton.addEventListener('click', toggleMenu);
-        
-function toggleMenu() {
-const menu = document.getElementById('nav')
-const linhaTopo = document.getElementById('linha-topo')
-const titulo = document.getElementById('titulo')
-
-    if (menu.classList.contains('hidden')){
-        menu.classList.remove('hidden')
-        titulo.classList.add('hidden')
-    } else  {
-        menu.classList.add('hidden')
-        titulo.classList.remove('hidden')
-    }
-                
-    }
-*/
+});
 
 //Faz com que as animações executem quando aparece a seção na tela
 //cria o observador
 const observadorEntradaDireita = new IntersectionObserver((entradas) => {
-    entradas.forEach((entrada) => {
-        if (entrada.isIntersecting) {
-            entrada.target.classList.add('animate-entrada-direita');
-            observadorEntradaDireita.unobserve(entrada.target)
-        } else {
-            entrada.target.classList.remove('animate-entrada-direita');
-        }
-    });
+  entradas.forEach((entrada) => {
+    if (entrada.isIntersecting) {
+      entrada.target.classList.add("animate-entrada-direita");
+      observadorEntradaDireita.unobserve(entrada.target);
+    } else {
+      entrada.target.classList.remove("animate-entrada-direita");
+    }
+  });
 });
 
 //Seleciona os elementos que serão observados e adicona o observador a eles
-const entradaDireita = document.querySelectorAll('#home-anim');
+const entradaDireita = document.querySelectorAll("#home-anim");
 entradaDireita.forEach((elemento) => {
-    observadorEntradaDireita.observe(elemento);
+  observadorEntradaDireita.observe(elemento);
 });
 
 const observadorFadeIn = new IntersectionObserver((entradas) => {
-    entradas.forEach((entrada) => {
-        if (entrada.isIntersecting) {
-            entrada.target.classList.add('animate-entrada-fade-in');
-            observadorFadeIn.unobserve(entrada.target)
-        } else {
-            entrada.target.classList.remove('animate-entrada-fade-in');
-        }
-    });
+  entradas.forEach((entrada) => {
+    if (entrada.isIntersecting) {
+      entrada.target.classList.add("animate-entrada-fade-in");
+      observadorFadeIn.unobserve(entrada.target);
+    } else {
+      entrada.target.classList.remove("animate-entrada-fade-in");
+    }
+  });
 });
 
-const FadeIn = document.querySelectorAll('#projetos-anim, .obj-home, #violino, #sax, #guitarra, #violao');
+const FadeIn = document.querySelectorAll(
+  "#projetos-anim, .obj-home, #violino, #sax, #guitarra, #violao"
+);
 FadeIn.forEach((elemento) => {
-    observadorFadeIn.observe(elemento);
+  observadorFadeIn.observe(elemento);
 });
-
-
 
 // Texto dos instrumentos | Adiciona fundo ao texto vindo do atributo "fundo" das imagens
-const imagens = document.querySelectorAll('img[data-txt]'); // Seleciona imagens com o atributo data-txt
-const espaco = document.getElementById('caixa');
+const imagens = document.querySelectorAll("img[data-txt]"); // Seleciona imagens com o atributo data-txt
+const espaco = document.getElementById("caixa");
 imagens.forEach((imagem) => {
-    imagem.addEventListener('mouseover', () => {
-        const texto = imagem.getAttribute('data-txt'); // Obtém o texto do atributo data-txt
-        espaco.textContent = texto; // Define o texto no elemento 'caixa'
-        espaco.classList.remove('hidden', 'overflow-hidden'); // Torna o elemento visível
-        espaco.style.backgroundImage = 'url(' + imagem.getAttribute('fundo') + ')';
-        espaco.classList.add('bg-center', 'bg-no-repeat');
-    });
+  imagem.addEventListener("mouseover", () => {
+    const texto = imagem.getAttribute("data-txt"); // Obtém o texto do atributo data-txt
+    espaco.textContent = texto; // Define o texto no elemento 'caixa'
+    espaco.classList.remove("opacity-0", "overflow-hidden"); // Torna o elemento visível
+    espaco.style.backgroundImage = "url(" + imagem.getAttribute("fundo") + ")";
+    espaco.classList.add("bg-center", "bg-no-repeat");
 
-    imagem.addEventListener('mouseout', () => {
-        espaco.classList.add('hidden'); // Oculta o elemento quando o mouse sai
-    });
+    // animação entrada
+    let opacidade = 0;
+    function fadeIn() {
+      opacidade += 0.02;
+      espaco.style.opacity = opacidade;
+      if (opacidade < 1) {
+        requestAnimationFrame(fadeIn);
+      }
+    }
+    fadeIn();
+  });
+
+  imagem.addEventListener("mouseout", () => {
+    // animação saída
+    let opacidade = 1;
+    function fadeOut() {
+      opacidade -= 0.05;
+      espaco.style.opacity = opacidade;
+      if (opacidade > 0) {
+        requestAnimationFrame(fadeOut);
+      }
+    }
+    fadeOut();
+  });
 });
 
 // Animação instrumentos
-        const observadorInstrumentos = new instersectionObserver((entries) => {
-            entries.forEach((entry) => {
-                if(entry.isIntersecting) {
-            entry.target.classList.add('animate-instrumentos')
-        } else {
-            entry.target.classList.remove('animate-instrumentos')
-                }
-            });
-        });
-
-        const instrumentos = document.querySelectorAll('.instrumentos')
-        instrumentos.forEach((elemento) => {
-            observadorInstrumentos.observe(elemento);
-        });
-
-
+const observadorInstrumentos = new instersectionObserver((entries) => {
+  entries.forEach((entry) => {
+    if (entry.isIntersecting) {
+      entry.target.classList.add("animate-instrumentos");
+    } else {
+      entry.target.classList.remove("animate-instrumentos");
+    }
+  });
+});
